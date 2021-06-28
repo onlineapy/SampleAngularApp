ce7e6acd4ee1163aad555fe40af5c6faf8a0b1d2
Apan Mittal <onlineapy@gmail.com>
2021-06-28 06:37:04 +0000

    First Commit Comment

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+todo:Todos[];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
